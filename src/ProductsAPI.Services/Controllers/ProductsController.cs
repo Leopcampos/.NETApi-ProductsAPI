3bc1dab67025dbe148310cab28d6a413065d23c4
Leo Campos <leopcampos85@gmail.com>
2024-03-02 14:59:25 -0300

    feat: Criando a camada de application.

---
+++ src/ProductsAPI.Services/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Application.Models.Commands;
+using ProductsAPI.Application.Models.Queries;
+
+namespace ProductsAPI.Services.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ProductsQuery), 201)]
+    public IActionResult Post([FromBody] ProductsCreateCommand command)
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    [ProducesResponseType(typeof(ProductsQuery), 200)]
+    public IActionResult Put([FromBody] ProductsUpdateCommand command)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(ProductsQuery), 200)]
+    public IActionResult Delete(Guid id)
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<ProductsQuery>), 200)]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(ProductsQuery), 200)]
+    public IActionResult Get(Guid? id)
+    {
+        return Ok();
+    }
+}
